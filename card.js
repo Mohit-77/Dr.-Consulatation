69607579107a166a8e19b4bd6866c905e743c947
Mohit-77 <mohits3752@gmail.com>
2021-08-23 12:23:42 +0530

    initial commit

---
+++ card.js
@@ -0,0 +1,18 @@
+conts card=[
+    {
+       textG:"Doctors" ,
+       textH:"Book an Appointment" 
+    },
+    {
+        textG:"Consult" ,
+        textH:"With top doctors"   
+    },
+    {
+        textG:"Pharmacy" ,
+        textH:"Medical Products"    
+    },
+    {
+        textG:"Diagnostics" ,
+        textH:"Test and Check-up"    
+    }
+]
